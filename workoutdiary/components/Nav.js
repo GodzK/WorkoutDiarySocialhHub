82dad204d7e1ef1d1716adf872a1a9f173f64bf4
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-04-30 23:26:41 +0700

    set up project for day1

---
+++ workoutdiary/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Nav.css'
+function Nav() {
+  return (
+    <div>
+        <h1 className='text-green-500'>This is Nav bar</h1>
+    </div>
+  )
+}
+
+export default Nav
