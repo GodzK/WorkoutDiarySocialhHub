839e2713817b3c52f950952bd10bc8e12497c0da
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-05-01 12:39:31 +0700

    finish navbar

--- workoutdiary/app/page.js
+++ workoutdiary/app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Nav from "@/components/Nav"
 export default function Home() {
   return (
-       <>
+       <div className="main-container">
        <Nav/>
      <h1>this is index</h1>  
-       </>
+       </div>
   );
 }
--- workoutdiary/components/Nav.js
+++ workoutdiary/components/Nav.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import './Nav.css'
+import './nav.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faChevronCircleDown, faComment, faDumbbell, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 function Nav() {
   return (
-    <div>
-        <h1 className='text-green-500'>This is Nav bar</h1>
-    </div>
+  <header className='header'>
+    <a className='logo'>
+    <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>workout diary
+    </a>
+    <nav className='navbar'>
+      <a href=""><FontAwesomeIcon icon={faPlusCircle}></FontAwesomeIcon></a>
+       <a href=""><FontAwesomeIcon icon={faBell}></FontAwesomeIcon></a>
+      <a><FontAwesomeIcon icon={faComment}></FontAwesomeIcon></a> 
+      <a href=""><FontAwesomeIcon icon={faChevronCircleDown}></FontAwesomeIcon></a> 
+    </nav>
+    </header>
   )
 }
 
