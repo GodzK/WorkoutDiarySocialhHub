d7ab027a948493a1d7c079b08650a098534c2d06
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2024-05-03 13:32:45 +0700

    finish profile page

--- workoutdiary/app/User/page.js
+++ workoutdiary/app/User/page.js
@@ -22,36 +22,84 @@ import Noti from "@/components/Notification/Noti"
 function Upro() {
   return (
     <div>
-       <Nav>
+      <Nav>
         <NavItem>
-         
-          <ChatDropdown>
-          </ChatDropdown>
-          <DropdownMenu>
-          </DropdownMenu>
-         </NavItem>
-        </Nav>
+          <ChatDropdown></ChatDropdown>
+          <DropdownMenu></DropdownMenu>
+        </NavItem>
+      </Nav>
       <div>
-      <h1 style={{color:"black"}}> Hi there</h1>
-       <h1 style={{color:"black"}}> Hi there</h1>
-       <h1 style={{color:"black"}}> Hi there</h1>
-        </div>
+        <h1 style={{color:"black"}}> Hi there</h1>
+        <h1 style={{color:"black"}}> Hi there</h1>
+        <h1 style={{color:"black"}}> Hi there</h1>
 
-  <div className='profile-container'>
-  <div className='profile'>
-    <div className='profile-header'>
-      <div className='profile-pic'>
-  <Image src={usericon}/>
       </div>
-      <h1>Phakaphol Dherachaisuphakij</h1>
-      <p>Front-end Blogger/Author/Front-end Developer from King Mongkut's University of Technology Thonburi (KMUTT)</p>
+
+      <div className='profile-container' style={{paddingTop:"25rem"}}>
+        <div className='profile'>
+          <div className='profile-header'>
+            <div className='profile-pic'>
+              <Image src={usericon} alt="Profile Picture" />
+            </div>
+            <h1>Phakaphol Dherachaisuphakij</h1>
+            <p>Front-end Blogger/Author/Front-end Developer from King Mongkut's University of Technology Thonburi (KMUTT)</p>
+          </div>
+          
+          <div className="useful-content">
+            <h2>Connect with me:</h2>
+            <ul>
+              <li>
+                <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-facebook-square"></i> Facebook
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/yourpage" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-twitter-square"></i> Twitter
+                </a>
+              </li>
+             
+            </ul>
+            <h2>Recent Posts:</h2>
+            <ul>
+              <li>
+                <a href="link-to-recent-post-1">Recent Post 1</a>
+              </li>
+              <li>
+                <a href="link-to-recent-post-2">Recent Post 2</a>
+              </li>
+        
+            </ul>
+          </div>
+
+          <div className="extra-content">
+            <div className="card">
+              <h1 style={{fontSize:"1.5rem" , color:"black"}} >Photos</h1> 
+              <div className="image-container">
+                <Image src={usericon} width={100} height={100} alt="Image 1"/>
+                <Image src={usericon} width={100} height={100} alt="Image 2"/>
+                <Image src={usericon} width={100} height={100} alt="Image 3"/>
+                <Image src={usericon} width={100} height={100} alt="Image 4"/>
+                <Image src={usericon} width={100} height={100} alt="Image 5"/>
+                <Image src={usericon} width={100} height={100} alt="Image 6"/>
+                <Image src={usericon} width={100} height={100} alt="Image 7"/>
+                <Image src={usericon} width={100} height={100} alt="Image 8"/>
+                <Image src={usericon} width={100} height={100} alt="Image 9"/>
+              </div>
+            </div>
+            <div className="card">
+              <div className="intro">
+                <h2 style={{color:"black"}}>Relationship Status : Single</h2>
+                <p style={{color:"black"}}>Place: Bangkok, Thailand</p>
+                <p style={{color:"black"}}>Edit Details <button>EDIT</button></p>
+                <p style={{color:"black"}}>Featured: <button>Add Featured</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-    
-    </div>
-  </div>
-</div>
   )
 }
 
-export default Upro
+export default Upro;
